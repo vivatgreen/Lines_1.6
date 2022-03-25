ab344daa56b7267661ad51dc16562eb98516d1cf
vivatgreen <grindsasha@bk.ru>
2022-03-25 11:30:20 +0300

    Строки!

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,24 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+task3();
     }
+    public static void task1() {
+        String firstName = "Иванов ";
+        String middleName = "Иван ";
+        String lastName = "Иванович";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+    public static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName1);
+    }
+    public static void task3(){
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+    }
+
 }
